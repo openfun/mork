9ad3e23a14aabed608405bc9605264b8a86ce532
Wilfried BARADAT <wilfried.baradat@fun-mooc.fr>
2024-11-05 17:14:20 +0100

    🗃️(mork) add created_at and updated_at fields to all models
    Adding `created_at` and `updated_at` fields to monitor entries in all database
    tables.

--- src/app/mork/migrations/env.py
+++ src/app/mork/migrations/env.py
@@ -18,7 +18,7 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    fileConfig(config.config_file_name, disable_existing_loggers=False)
 
 # Set Database URL from Morks configuration
 config.set_main_option("sqlalchemy.url", str(settings.DB_URL))
